21e9aad7881470827869ea2aae13fe434eeac425
Daniel Kussin <daniel.kussin@kussin.de>
2024-05-14 10:30:55 +0200

    This commit refs #64301: Added enhanced ip check

--- src/source/banned_ips.php
+++ src/source/banned_ips.php
@@ -3,18 +3,19 @@
 include_once dirname(__FILE__) . "/banned_ips.inc.php";
 
 // GET REMOTE IP
+$sRemoteIp = $_SERVER['REMOTE_ADDR'];
+
+// PROXY CHECK
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $sRemoteIp = $_SERVER['HTTP_CLIENT_IP'];
-
 } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $sRemoteIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
-
-} else {
-    $sRemoteIp = $_SERVER['REMOTE_ADDR'];
+    // TRIM MULTIPLE IPS
+    $aRemoteIpList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+    $sRemoteIp = trim($aRemoteIpList[0]);
 }
 
 // CHECK REMOTE IP
-if (in_array($sRemoteIp, $aBannedIps)) {
+if (in_array(trim($sRemoteIp), $aBannedIps)) {
     // BLOCK IP
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this page.');
